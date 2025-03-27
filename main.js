a5fd3027f8a570cebeb9eb0734290988d9b93703
msenginoglu <msengiloglu@gmail.com>
2025-03-28 01:29:05 +0300

    * Hero eklendi. * Hakkımızda eklendi. * Yukarı çık butonu eklendi.

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Mobile Menu Elements
   const hamburger = document.querySelector(".hamburger");
   const mobileMenu = document.querySelector(".mobile-menu");
   const mobileMenuOverlay = document.querySelector(".mobile-menu-overlay");
-  const closeMenu = document.querySelector(".close-menu"); // Yeni kapatma butonu
+  const closeMenu = document.querySelector(".close-menu");
   const body = document.body;
 
+  // Go Up Button Element
+  const goUpBtn = document.getElementById("go-up-btn");
+
+  // Mobile Menu Functions
   function toggleMobileMenu() {
     mobileMenu.classList.toggle("active");
     mobileMenuOverlay.classList.toggle("active");
     body.classList.toggle("menu-open");
   }
 
+  // Go Up Button Functions
+  function toggleGoUpButton() {
+    if (window.scrollY > 500) {
+      goUpBtn.classList.add("active");
+    } else {
+      goUpBtn.classList.remove("active");
+    }
+  }
+
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+
+  // Mobile Menu Event Listeners
   if (hamburger) {
     hamburger.addEventListener("click", toggleMobileMenu);
   }
@@ -21,5 +43,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
   if (mobileMenuOverlay) {
     mobileMenuOverlay.addEventListener("click", toggleMobileMenu);
+  }
+
+  // Go Up Button Event Listeners
+  if (goUpBtn) {
+    window.addEventListener("scroll", toggleGoUpButton);
+    goUpBtn.addEventListener("click", scrollToTop);
   }
 });
